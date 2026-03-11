70654eb5975a8427d424ccbd9d27efa9e24603e2
Priyanshi Panchal <aapka-email@example.com>
2026-03-11 16:46:05 +0530

    Ready for deployment with gunicorn

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn heart_project.wsgi
--- heart_project/settings.py
+++ heart_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-od+4da%va^_*=i%(h43#f(me(7fu0)jcn76dsccl%(6dz+@-^5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
